4902c75a973c9c3271a1985354d012cf0c4e1512
FernandoNieto2002 <fernando.nieto@cua.uam.mx>
2025-11-19 16:10:21 -0600

    Add files via upload

---
+++ miproyecto/src/main/java/com/lab/miproyecto/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.lab.miproyecto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye() {
+        return "goodbye";
+    }
+}
